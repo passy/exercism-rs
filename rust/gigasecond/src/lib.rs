6b2d3e0a614fea28456d2411321d880a18dd21b6
Pascal Hartig <phartig@rdrei.net>
2016-06-04 22:29:57 +0100

    Oh, that was unnecessarily verbose

--- rust/gigasecond/src/lib.rs
+++ rust/gigasecond/src/lib.rs
@@ -1,7 +1,5 @@
 extern crate chrono;
 
-use std::ops::Add;
-
 pub fn after(start: chrono::DateTime<chrono::UTC>) -> chrono::DateTime<chrono::UTC> {
-    start.add(chrono::Duration::seconds(1_000_000_000))
+    start + chrono::Duration::seconds(1_000_000_000)
 }
